08a5e2fe8541b735af0550a58bc2c324687fba85
Bielf19 <biel.fernandez@students.salle.url.edu>
2019-04-11 08:18:21 +0200

    Sasse estas bonissim

---
+++ src/Model/BaseDades/DAO/UserDAO.java
@@ -0,0 +1,24 @@
+package Model.BaseDades.DAO;
+
+import Model.BaseDades.DataBase;
+import Model.Usuari;
+
+public class UserDAO {
+
+    private static String codiAmistat;
+
+    public void addUser(Usuari user) {
+        String query = "INSERT INTO Usuaris(nomUsuari, password, correu, codiAmistat) VALUES ('"+user.getLogin().getNomUsuari()+"', '"+user.getLogin().getPassword()+"', '"+user.getLogin().getCorreu()+"', '"+codiAmistat+"');";
+        System.out.println(query);
+        DataBase.getInstance().insertQuery(query);
+    }
+
+
+    public Usuari getLastUser() {
+        Usuari user = new Usuari();
+        String query = "SELECT COUNT (user_id) FROM Usuaris";
+    }
+    private void generaCodiAmistat(){
+
+    }
+}
