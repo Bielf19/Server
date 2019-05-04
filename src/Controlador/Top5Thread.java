752a43a7cadaecd6d96b9faa1ac78d0dfc5346c5
Bielf19 <biel.fernandez@students.salle.url.edu>
2019-05-04 14:44:35 +0200

    Sasse <3

---
+++ src/Controlador/Top5Thread.java
@@ -0,0 +1,35 @@
+package Controlador;
+
+import Model.Model;
+import Model.Song;
+import Vista.Top5;
+import Vista.Window1;
+
+import java.util.LinkedList;
+
+public class Top5Thread extends Thread {
+
+    private Model model;
+    private Window1 window1;
+
+    public Top5Thread (Model model, Window1 window1) {
+        this.window1 = window1;
+        this.model = model;
+    }
+
+    @Override
+    public synchronized void run() {
+        while (true) {
+            System.out.println("PROVA11");
+            LinkedList<Song> top5 = model.getTop5();
+            window1.setTop5(top5);
+            for (int i = 0; i < top5.size(); i++) {
+                System.out.println(top5.get(i).getTitol());
+            }
+            System.out.println("PROVA12");
+            //window1.setTop5(top5);
+            //window1.generaTaulaTop5();
+            window1.updateTop5();
+        }
+    }
+}
