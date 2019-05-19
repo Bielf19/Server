1a6268ce4423882c35fb9e6a5ee60cf94e4f3718
Enricsa98 <enric.sasselli@gmail.com>
2019-05-19 17:53:33 +0200

    T'estimo ULTIMA ACTUALITZACIÓ 19/05/2019

--- src/Vista/JTableButtonRenderer.java
+++ src/Vista/JTableButtonRenderer.java
@@ -4,6 +4,10 @@ import javax.swing.*;
 import javax.swing.table.TableCellRenderer;
 import java.awt.*;
 
+/**
+ * La classe JTableButtonRenderer s'utilitzarà per a introduir un JButton a una JTable.
+ */
+
 public class JTableButtonRenderer implements TableCellRenderer {
     @Override public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         JButton button = (JButton)value;
